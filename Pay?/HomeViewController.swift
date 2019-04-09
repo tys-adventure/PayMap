befff49bebb21187006cc8faca44388ad72e384a
Michael Ocasio <michael.a.ocasiofigueroa@gmail.com>
2019-04-08 21:43:19 -0400

    Addition: Load Up Screen
    - Addition of the logo to the load screen and
    -Added the HomeViewController to add code for a primary screen with possible options.

---
+++ Pay?/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  Pay?
+//
+//  Created by Michael Ocasio on 4/8/19.
+//  Copyright © 2019 Tyler P Admin. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController : UIViewController {
+    
+    
+    
+    
+}
